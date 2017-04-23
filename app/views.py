a4132352d633cdfa8cc113a0606eec5475c3d069
AlexPutrya <alex.putrya@gmail.com>
2017-04-23 13:03:22 +0300

    Start project

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from flask import render_template, url_for, request
+
+from app import app
+
+
+@app.route('/')
+def index():
+    return render_template('catalog.html')
+
+@app.route('/catalog')
+def catalog():
+    return render_template('catalog.html')
