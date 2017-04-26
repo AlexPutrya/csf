90b933e6797c70ddc88ac54d880b1eab5b9d0264
AlexPutrya <alex.putrya@gmail.com>
2017-04-26 22:37:53 +0300

    View products onclick button catalog group

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, redirect, flash, request
+from flask import render_template, url_for, redirect, flash, request, jsonify
 from app import app
 
 
@@ -8,10 +8,28 @@ def index():
 
 @app.route('/catalog')
 def catalog():
-    return render_template('catalog.html')
+    groups = [{'id': 0, 'name': "Напитки"},  {'id': 1, 'name': "Бургеры"}, {'id': 2, 'name': "Салаты"}]
+    dishes = [
+        [
+            {'id': 0, 'name': 'Бургер Американский'},
+            {'id': 0, 'name': 'Бургер Европейски'},
+            {'id': 0, 'name': 'Бургер Азиатский'},
+        ]
+    ]
+    return render_template('catalog.html', groups = groups)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.form.get('email'):
         return request.form.get('email')
     return render_template('login.html', title = "Авторизация")
+
+
+@app.route('/dishes', methods=['GET', 'POST'])
+def dishes():
+    dishes = { "products": [
+        {'id': 0, "name": "Бургер Американский"},
+        {'id': 1, "name": "Бургер Азиатский"},
+        {'id': 2, "name": "Европейский"}
+    ]}
+    return jsonify(dishes)
