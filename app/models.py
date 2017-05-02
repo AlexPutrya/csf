dcd03b623abc4b45a6988ea9e9c1cfbdfbc15b36
AlexPutrya <alex.putrya@gmail.com>
2017-05-02 12:20:22 +0300

    Create product model and table

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class User(db.Model):
     password = db.Column(db.String(150))
     role = db.Column(db.SmallInteger, default=ROLE_USER)
 
-    def __init__(self, login, password, role = 0):
+    def __init__(self, login, password, role = ROLE_USER):
         self.login = login
         self.password = password
         self.role = role
@@ -19,10 +19,24 @@ class User(db.Model):
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(150))
+    name = db.Column(db.String(150), nullable=False)
 
     def __init__(self, name):
         self.name = name
 
     def __repr__(self):
-        return '<Category %r>' %self.name
+        return '<Category %r>' % self.name
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_category = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, id_category, name, price):
+        self.id_category = id_category
+        self.name = name
+        self.price = price
+
+    def __repr__(self):
+        return '<Product %r>' % self.name
