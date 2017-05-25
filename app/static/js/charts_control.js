98807632edd0fb2999612bfad619a21f98decc47
AlexPutrya <alex.putrya@gmail.com>
2017-05-25 09:25:20 +0300

    Slow animation in initialize bar

--- app/static/js/charts_control.js
+++ app/static/js/charts_control.js
@@ -7,35 +7,6 @@ $(document).ready(function(){
   statistic_month(year, month);
   statistic_year(year-1);
 
-  var year = document.getElementById("yearChart");
-  var yearChart = new Chart(year, {
-      type: 'bar',
-      data: {
-          labels: [],
-          datasets: [{
-              label: 'Сумма, грн.',
-              data: [],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
-      }
-  });
-
   // график по месяцам
   var month = document.getElementById("monthChart");
   var monthChart = new Chart(month, {
@@ -76,14 +47,41 @@ $(document).ready(function(){
       type: "GET",
       success: function(data){
         var price = []
-        yearChart.data.labels =[]
+        var labels =[]
         $.each(data, function(key,value){
           price[key-1] = value;
-          yearChart.data.labels.push(getMonthName(key));
+          labels.push(getMonthName(key));
         })
-        yearChart.data.datasets[0].data = price;
-        yearChart.update();
         $("#title_year").text("Статистика за "+year+" год");
+
+        var chart = document.getElementById("yearChart");
+        var yearChart = new Chart(chart, {
+            type: 'bar',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Сумма, грн.',
+                    data: price,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            }
+        });
       }
     });
   }
