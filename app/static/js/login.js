edee812fca7ee2583a87627e088e24ca3ebc3cd7
AlexPutrya <alex.putrya@gmail.com>
2017-05-27 22:09:05 +0300

    Add login and password validation

---
+++ app/static/js/login.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  // проверяем чтоб были не пустыми поля перд отправкой
+  $("#submit").click(function(e){
+    var name = $("input[name='login']").val();
+    var password = $("input[name='password']").val();
+      if(name && password){
+        return true;
+      }else{
+        if(password == "" && name == ""){
+          $("input[name='password']").parent().addClass("has-error");
+          $("input[name='login']").parent().addClass("has-error");
+        }else if(password == ""){
+          $("input[name='password']").parent().addClass("has-error");
+        }else if (name == "") {
+          $("input[name='login']").parent().addClass("has-error");
+        }
+        return false;
+      }
+  });
+
+  // при нажатии в форму убираем красный цвет
+  $("input").focus(function(){
+    $(this).parent().removeClass("has-error");
+  });
+
+});
