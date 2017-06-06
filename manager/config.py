7fa0f805f4b0c5ff4d4aa86ab7fe6e0b0f7d23bd
AlexPutrya <alex.putrya@gmail.com>
2017-06-06 12:01:14 +0300

    Using gunicorn and mysql

---
+++ manager/config.py
@@ -0,0 +1,14 @@
+import os
+
+DATABASE = 'test'
+PASSWORD = 'supersecure'
+USER = 'root'
+HOSTNAME = 'database'
+
+
+SQLALCHEMY_DATABASE_URI = 'mysql://%s:%s@%s/%s'%(USER, PASSWORD, HOSTNAME, DATABASE)
+
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(basedir, 'database.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = 'manager-python'
